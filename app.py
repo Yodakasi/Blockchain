aeaee953091ff50dde2d138899af7c1cc88d79d9
yodak <yodak@kombajnPorzogi.com>
2018-05-30 10:28:10 +0200

    simple mining and transaction system

---
+++ app.py
@@ -0,0 +1,134 @@
+from flask import *
+from flask_mysqldb import MySQL
+import hashlib
+import random
+from pprint import pprint
+from blockchain import block, blockChain
+import os
+import threading
+
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.config['SESSION_COOKIE_HTTPONLY'] = False
+mysql = MySQL(app)
+
+# MySQL configurations
+mysql.app.config['MYSQL_USER'] = 'root'
+mysql.app.config['MYSQL_PASSWORD'] = 'toor'
+mysql.app.config['MYSQL_DB'] = 'dupcoin'
+mysql.app.config['MYSQL_HOST'] = 'localhost'
+
+print("Initialization:")
+actualBlockChain = blockChain(4)
+
+def checkChainValidity():
+    if actualBlockChain.validateChain():
+        return True
+    else:
+        actualBlockChain.chainReset()
+        print("wyjebalem chujkow")
+        return False
+
+def setInterval(func, sec):
+    def funcWrapper():
+        setInterval(func, sec)
+        func()
+    t = threading.Timer(sec, funcWrapper)
+    t.start()
+    return t
+
+def hash(notHashed):
+    salt = ''.join(chr(random.randint(34,126)) for i in range(8))
+    hash = hashlib.sha256((notHashed + salt).encode())
+    return hash.hexdigest() + '!' + salt
+
+def verifyhash(hashed, password):
+    splited = hashed.split("!")
+    if hashlib.sha256((str(password) + splited[1]).encode()).hexdigest() == splited[0]:
+        return True
+    return False
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.form.get('login'):
+        login = request.form.get('login')
+        password = request.form.get('password')
+        try:
+            conn = mysql.connection
+            cur = conn.cursor()
+            cur.execute('''SELECT password FROM `users` WHERE login=%s''', (login))
+            if verifyhash(cur.fetchall()[0][0], password):
+                session['user'] = login
+                return redirect(url_for('user'))
+            else:
+                message = "Złe hasło lub login <a href='/'>wróć</a>"
+        except:
+            message = "Chuja tam <a href='/'>wróć</a>"
+            print(login)
+            print(password)
+        return render_template_string(message)
+    else:
+        return render_template('login.html')
+
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    if request.form.get('loginreg'):
+        login = request.form.get('loginreg')
+        email = request.form.get('emailreg')
+        password = hash(str(request.form.get('passwordreg')))
+        try:
+            conn = mysql.connection
+            cur = conn.cursor()
+            cur.execute('''SELECT * FROM `users` WHERE login=%s''', (login))
+            elo = len(str(cur.fetchall()))
+            if elo > 2:
+                message = "login zajeety <a href='/'>wróć</a>"
+            else:
+                cur.execute('''INSERT INTO `users` (`id`, `login`, `email`, `password`) VALUES (NULL, %s, %s, %s)''', (login, email, password))
+                conn.commit()
+                message = "Rejetracja powiodla sie <a href='/'>wróć</a>"
+        except:
+            message = "Rejetracja nie powiodla sie <a href='/'>wróć</a>"
+        return render_template_string(message)
+    else:
+        return render_template('register.html')
+
+@app.route('/user', methods=['POST', 'GET'])
+def user():
+    if 'user' in session:
+        user = session['user']
+        balance1 = actualBlockChain.getAdressBalance(user)
+        print(actualBlockChain.chain[-1].__dict__)
+        print(actualBlockChain.validateChain())
+        if request.form.get('adress'):
+            amount = int(request.form.get('amount'))
+            if int(actualBlockChain.getAdressBalance(user)) > amount and amount > 0 and request.form.get('adress') != user:
+                actualBlockChain.newTransaction(user, request.form.get('adress'), request.form.get('amount'))
+        if int(actualBlockChain.getAdressBalance(user)) > 1000000 and actualBlockChain.validateChain():
+            balance1 = "RSCTF_{F1RST_MILLION$$_U_H$VE_TO_STEAL}"
+        return render_template('user.html', username=user, balance=balance1)
+    else:
+        return redirect('/')
+
+@app.route('/sendblock', methods = ['POST'])
+def postJsonHandler():
+    content = request.get_json()
+    actualBlockChain.addBlockFromClient(json.dumps(content, default=lambda o: o.__dict__))
+    return 'JSON posted'
+
+@app.route('/getblock', methods = ['GET'])
+def getJsonHandler():
+    if 'user' in session:
+        user = session['user']
+        if len(actualBlockChain.transactionsQueue) > 0:
+            return json.dumps(actualBlockChain.newBlockSchema(user), default=lambda o: o.__dict__)
+    return render_template_string("nope"), 204
+
+if __name__ == "__main__":
+    setInterval(checkChainValidity, 10)
+    app.run(host='0.0.0.0')
---
+++ blockchain.py
@@ -0,0 +1,134 @@
+import hashlib
+from collections import namedtuple
+from time import time
+import json
+
+class block:
+    def __init__(self, previusHash, transactions, difficulty):
+        self.date = time()
+        self.previusHash = previusHash
+        self.transactions = transactions
+        self.nonce = 0
+        self.difficulty = difficulty
+        self.hash = ''
+        #self.hash = self.mineHash()
+
+    def calculateHash(self):
+        transactionsString = ""
+        if type(self.transactions) is not int:
+            for i in self.transactions:
+                transactionsString += i.fromAdress
+                transactionsString += ","
+                transactionsString += i.toAdress
+                transactionsString += ","
+                transactionsString += str(i.amount)
+                transactionsString += ","
+            transactionsString = transactionsString[0 : -1]
+        else:
+            transactionsString = str(self.transactions)
+        strToHash = (self.previusHash + str(self.date) + transactionsString + str(self.nonce)).encode('utf-8')
+        return hashlib.sha256(strToHash).hexdigest()
+
+    def mineHash(self):
+        hashed = ''
+        while hashed[0 : self.difficulty] != "{}".format('0' * self.difficulty):
+            self.nonce += 1
+            hashed = self.calculateHash()
+        return hashed
+
+    def addBlock(self, date, previusHash, transactions, nonce, difficulty, hash):
+        self.date = date
+        self.previusHash = previusHash
+        self.transactions = []
+        transactionScheme = namedtuple('transaction', 'fromAdress toAdress amount')
+        for x in transactions:
+            transaction = transactionScheme(fromAdress=x[0], toAdress=x[1], amount=int(x[2]))
+            self.transactions.append(transaction)
+        self.nonce = nonce
+        self.difficulty = difficulty
+        self.hash = hash
+
+
+class blockChain:
+    def __init__(self, difficulty):
+        self.chain = []
+        self.transactionsQueue = []
+        self.difficulty = difficulty
+        self.mineReward = 1
+        self.firstBlock()
+        #for client purposes mining first block
+        self.chain[-1].hash = self.chain[-1].mineHash()
+
+
+    def firstBlock(self):
+        self.chain.append(block('0', 0, self.difficulty))
+
+    def mineBlock(self, adress):
+        if len(self.transactionsQueue) < 1:
+            return 0
+        self.newTransaction('minedReward', adress, self.mineReward)
+        self.chain.append(block(self.chain[-1].hash, self.transactionsQueue, self.difficulty))
+
+    def newBlockSchema(self, adress):
+        newTrans = True
+        for x in self.transactionsQueue:
+            if 'minedReward' in x[0]:
+                newTrans = False
+        if newTrans:
+            self.newTransaction('minedReward', adress, self.mineReward)
+        return block(self.chain[-1].hash, self.transactionsQueue, self.difficulty)
+
+    def validateChain(self):
+        for i in range(len(self.chain)):
+            if self.chain[i].calculateHash() != self.chain[i].hash:
+                return False
+        return True
+
+    def newTransaction(self, froms, to, howMuch):
+        transactionScheme = namedtuple('transaction', 'fromAdress toAdress amount')
+        transaction = transactionScheme(fromAdress=froms, toAdress=to, amount=int(howMuch))
+        self.transactionsQueue.append(transaction)
+
+    def getAdressBalance(self, adress):
+        balance = 100
+        for i in range(1, len(self.chain)):
+            for j in range(0, len(self.chain[i].transactions)):
+                if self.chain[i].transactions[j].fromAdress == adress:
+                    balance -= self.chain[i].transactions[j].amount
+                if self.chain[i].transactions[j].toAdress == adress:
+                    balance += self.chain[i].transactions[j].amount
+        return balance
+
+    def addBlockFromClient(self, content):
+        content = json.loads(content)
+        for x in self.chain:
+            if x.hash == content['hash']:
+                return False
+        self.chain.append(block(None, None, None))
+        self.chain[-1].addBlock(content['date'], content['previusHash'], content['transactions'], content['nonce'], content['difficulty'], content['hash'])
+        self.transactionsQueue = []
+
+    def chainReset(self):
+        for i in range(len(self.chain)):
+            if self.chain[i].calculateHash() != self.chain[i].hash:
+                self.chain = self.chain[0:i]
+                return True
+        return False
+
+def main():
+    print("Initialization:")
+    m = blockChain(2)
+    m.newTransaction('llll', 'sdfsdf', 150)
+    m.newTransaction('llll', 'sdfsdf', 50)
+    m.mineBlock('dupsko')
+    print(m.chain[-1].transactions.fromAdress)
+    m.newTransaction('sdfsdf', 'llll', 100)
+    m.mineBlock('dupsko')
+    m.mineBlock('lol')
+    m.mineBlock('dupsko')
+    print(len(m.transactionsQueue))
+    print(m.getAdressBalance('dupsko'))
+
+
+if __name__ == "__main__":
+    main()
